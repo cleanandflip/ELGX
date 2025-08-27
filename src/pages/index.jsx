4191c09dd642e03889d9084c419e256b03b8a574
FLOMAIN <MYFLOMAIN@GMAIL.COM>
2025-08-27 15:18:12 -0400

    3

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,6 +7,7 @@ import Quote from "./Quote";
 import Drivers from "./Drivers";
 
 import Contact from "./Contact";
+import PlasmicHost from "./plasmic-host";
 
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
@@ -40,21 +41,24 @@ function PagesContent() {
     const location = useLocation();
     const currentPage = _getCurrentPage(location.pathname);
     
+    const isPlasmic = location.pathname === '/plasmic-host';
+    
+    if (isPlasmic) {
+        return (
+            <Routes>
+                <Route path="/plasmic-host" element={<PlasmicHost />} />
+            </Routes>
+        );
+    }
+
     return (
         <Layout currentPageName={currentPage}>
             <Routes>            
-                
-                    <Route path="/" element={<Home />} />
-                
-                
+                <Route path="/" element={<Home />} />
                 <Route path="/Home" element={<Home />} />
-                
                 <Route path="/Quote" element={<Quote />} />
-                
                 <Route path="/Drivers" element={<Drivers />} />
-                
                 <Route path="/Contact" element={<Contact />} />
-                
             </Routes>
         </Layout>
     );
